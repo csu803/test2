2d950bae0ef7d26a74cd94e142f702f0fffc5c87
csu803 <369394337@qq.com>
2015-12-23 14:37:56 +0800

    闪退

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by chenfalei on 15/12/23.
+//  Copyright © 2015年 chenfalei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by chenfalei on 15/12/23.
+//  Copyright © 2015年 chenfalei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
