b7f4bb75d72f6ea4b579d57c8887e0078bdc5496
csu803 <369394337@qq.com>
2015-12-23 17:25:30 +0800

    a

--- test2/ViewController.m
+++ test2/ViewController.m
@@ -23,7 +23,7 @@
         NSLog(@"fuck");
         NSLog(@"fuck");
             NSLog(@"fuck2");
-            NSLog(@"fuck1");    
+            NSLog(@"fuck1");
 }
 
 - (void)didReceiveMemoryWarning {
